7cf6199092ee7f2ae01f57e625fd28c5ec3df75b
Sylvain GERARD <sylv.gerard@gmail.com>
2018-07-20 00:27:15 +0200

    Add smooth scrolling to anchors

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() title: string;
 
+  scrollToElement(id): void {
+    const el = document.getElementById(id);
+    console.log(el);
+    el.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+  }
+
   constructor() { }
 
   ngOnInit() {

721377fd20c4ec18c8219dc83a657fcd0b2ad48f
Sylvain GERARD <sylv.gerard@gmail.com>
2018-07-20 01:01:03 +0200

    Fix navbar buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() title: string;
 
+  currentTab = 'intro';
+
   scrollToElement(id): void {
     const el = document.getElementById(id);
     console.log(el);
     el.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+    history.pushState(null, null, '#' + id);
+    this.currentTab = id;
   }
 
   constructor() { }
